e40cd2524373cba2a23bc8165b8bac1dde2ea38c
leb90 <leandro.bisceglie@gmail.com>
2025-03-28 12:34:34 -0300

    add and fix ui

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.liquid',
+    './sections/**/*.liquid',
+    './snippets/**/*.liquid',
+    './layout/**/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+} 
